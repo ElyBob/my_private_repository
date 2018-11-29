a4b7b4e688d61acd7587027ab6296e752707d5e6
Calabrio\larry.louisiana <louisila@hotmail.com>
2018-11-28 20:38:00 -0600

    adding greendot branch

---
+++ GreenDot/execute.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LarryL
+ * Date: 11/28/2018
+ * Time: 5:59 PM
+ */
+ class execute
+ {
+     static public function greenstar() {
+         echo '<style> .dot { height: 125px; width: 125px; background-color: #0f0; border-radius: 50%; display: inline-block;}</style>';
+         echo '<div style="text-align:center"> <span class="dot"></span> </div>\' ;
+     }
+ }
