126e914d7a696e6d211d44178df667cbf5ee81e9
Calabrio\larry.louisiana <larry.louisiana@calabrio.com>
2018-11-28 19:45:36 -0600

    adding BlueStar class on BlueStar branch

---
+++ BlueStar/draw.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LarryL
+ * Date: 11/28/2018
+ * Time: 5:58 PM
+ */
+ class draw
+ {
+     static public function bluestar() {
+         echo '     __.-._' . '<br>';
+         echo '     \'-._"7\'' . '<br>' ;
+         echo '      /\'.-c' . '<br>';
+         echo '      |  /T' . '<br>';
+         echo '     _)_/LI' . '<br>';
+     }
+ }
+?>
